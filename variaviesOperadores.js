65dc6f664b9b73c46fc57819126d276f9a87ff90
Jilocunhaaa <ricardo.ocunha@a.ucb.br>
2025-02-27 11:08:06 -0300

    Add files via upload

---
+++ variaviesOperadores.js
@@ -0,0 +1,19 @@
+let num_a = 10;
+let num_b = 20;
+let num_c = num_a + num_b;
+
+let soma = (`${num_a} + ${num_b} = ${num_c}`);
+
+console.log(soma);
+
+num_c = num_a - num_b;
+
+console.log(`${num_a} - ${num_b} = ${num_c}`);
+
+num_c = num_a / num_b;
+
+console.log(`${num_a} / ${num_b} = ${num_c}`);
+
+num_c = num_a * num_b;
+
+console.log(`${num_a} * ${num_b} = ${num_c}`);
